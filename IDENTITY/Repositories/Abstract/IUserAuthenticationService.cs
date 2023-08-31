d5ff643c60b4d36acbe852182ab8a9a3121889bb
Pranav <sapkotapranav@gmail.com>
2023-08-31 20:47:58 +0545

    firstcommit

---
+++ IDENTITY/Repositories/Abstract/IUserAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using IDENTITY.Models.DTO;
+
+namespace IDENTITY.Repositories.Abstract
+{
+    public interface IUserAuthenticationService
+    {
+        Task<Status> LoginAsync(LoginModel model);
+        Task<Status> RegistrationAsync(RegistrationModel model);
+        Task LogoutAsync();
+
+    }
+}
